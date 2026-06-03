5e736d023012a4dd8d2aaee4f29a27c22e81c561
Jok <66626821+JOKleinGe1@users.noreply.github.com>
2026-06-03 15:45:40 +0200

    Update tb_riscv.vhd

--- tb_riscv.vhd
+++ tb_riscv.vhd
@@ -2,8 +2,7 @@
 -- riscv_simple.vhd
 -- Simplified RISC-V  (some RV32I instructions are missing) 
 -- Revision 2026-06-03 (jok & PhC) - IUT de CACHAN Dept GEii-1 - Univ Paris Saclay
--- Please see assembly source in src directory, run make 
--- and update the  binary code instruction (line 55-70) with content of src/imem_initial.vhd 
+-- Use this version for MODELSIM simulation 
 -- Last update : add the previous comments
 -- =====================================================
 
